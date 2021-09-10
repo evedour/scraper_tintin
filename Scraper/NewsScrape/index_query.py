3b51db97021afd25c0b6a94259b78f69148a3beb
Eva Ntourou <64539498+evedour@users.noreply.github.com>
2021-09-10 13:12:01 +0300

    Update index_query.py

--- Scraper/NewsScrape/index_query.py
+++ Scraper/NewsScrape/index_query.py
@@ -1,25 +1,53 @@
 from bs4 import BeautifulSoup
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
 
 
 def query(words):
+    # read index
     soup = BeautifulSoup(open('./Results/inverted_index.xml', 'r'), 'lxml')
-    for word in words:
+    with open('Results/lemmas.txt', 'r')as lemmas_in:
+        lemmas = [line.split(',') for line in lemmas_in.readlines()]
+
+    lemmatizer = WordNetLemmatizer()
+    stemmer = SnowballStemmer()
+
+    wghts = []
+    links = []
+
+    for wrd in words:
+        #lemmatize query
+        word = stemmer.stem(lemmatizer.lemmatize(wrd, 'v'))
+        for lem in lemmas:
+            if word in lem:
+                word = lem
+                break
+
         lemma = soup.findAll('lemma', {"name": f"{word.lower()}"})
+        # find query in lemmas and return the result based on tf-idf weight
         if len(lemma) == 0:
-            print(f'{word} not found in lemmas')
+            print(f'No matches found for {word} ')
             continue
         else:
-            weights = []
-            ids = []
+
             for document in lemma[0].find_all('document'):
                 if float(document.attrs['weight'])> 0.0:
-                    weights.append(document.attrs['weight'])
-                    ids.append(document.attrs['id'])
+                    wghts.append(document.attrs['weight'])
+                    links.append(document.attrs['id'])
+    if len(links) > 0:
+        zipped = zip(wghts, links)
+        sorted_pairs = sorted(zipped)
+        tuples = zip(*sorted_pairs)
+        wghts, links = [list(tuple) for tuple in tuples]
+        for id in links:
+            for is_same in links:
+                if is_same == id:
+                    wghts[links.index(id)] += wghts[links.index(is_same)]
+                    links.remove(is_same)
 
-            zipped = zip(weights, ids)
-            sorted_pairs = sorted(zipped)
-            tuples = zip(*sorted_pairs)
-            weights, ids = [list(tuple) for tuple in tuples]
-            print(f'Results for lemma {word}: \n')
-            for id in ids:
-                print(f'{id}\n')
+        print(f'Results: \n')
+        count = 0
+        for id in links:
+            count += 1
+            print(f'{id}\n')
+        print(f'Query returned {count} results')
