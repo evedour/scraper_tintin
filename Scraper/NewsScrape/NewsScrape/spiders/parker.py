243b37c3efdde8108017900057cc6259b8cb1ca4
Eva Ntourou <64539498+evedour@users.noreply.github.com>
2021-09-10 13:10:19 +0300

    Add files via upload

--- Scraper/NewsScrape/NewsScrape/spiders/parker.py
+++ Scraper/NewsScrape/NewsScrape/spiders/parker.py
@@ -7,16 +7,16 @@ class PeterParker(scrapy.Spider):
 
     def start_requests(self):
         sources = ['https://www.eureporter.co/feed/',
-                   'https://www.aljazeera.com/xml/rss/all.xml',
-                   'http://rss.cnn.com/rss/edition_world.rss',
-                   'http://feeds.feedburner.com/euronews/en/home/',
-                   'http://feeds.bbci.co.uk/news/world/rss.xml',
+                  'https://www.aljazeera.com/xml/rss/all.xml',
+                  'http://feeds.feedburner.com/euronews/en/home/',
+                  'http://feeds.bbci.co.uk/news/world/rss.xml',
                    'https://www.politico.eu/feed/',
                    'https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/world/rss.xml']
         for source in sources:
             yield scrapy.Request(url=source, callback=self.parse)
 
     def parse(self, response):
+        # for each source, scrape it and return the result in an .html file
         page = response.url.split('/')[-3]
         filename = f'../../Results/news_{page}.html'
         with open(filename, 'wb') as f:
