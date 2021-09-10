01fa03b833fba79e8e10e03c1973bf538d30e8a0
Eva Ntourou <64539498+evedour@users.noreply.github.com>
2021-09-10 13:12:47 +0300

    Update tokenizer.py

--- Scraper/NewsScrape/tokenizer.py
+++ Scraper/NewsScrape/tokenizer.py
@@ -3,6 +3,20 @@ import cleaner
 import MySQLdb
 import json
 from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+import re
+
+
+def insert_tokenized(tokens, key):
+    # insert tokenized article to db
+    tokenized_article = ""
+    for token in tokens:
+        tokenized_article += f'{token} '
+    db = MySQLdb.connect('localhost', 'root', '', 'articleDB', charset='utf8')
+    crs = db.cursor()
+    # care for typo in column
+    crs.execute(f"""UPDATE articles SET tokenized_aricle = \"{tokenized_article}\" WHERE id = \"{key}\"""")
+    db.commit()
 
 
 def get_articles():
@@ -10,6 +24,7 @@ def get_articles():
     db = MySQLdb.connect('localhost', 'root', '', 'articleDB', charset='utf8')
     crs = db.cursor()
     crs.execute("""SELECT link, article FROM articles""")
+
     return crs.fetchall()
 
 
@@ -26,10 +41,15 @@ def tokenize():
         # save links
         links.append(link)
         # use nltk to tokenize and tag article
-        tokens = nltk.word_tokenize(article)
-        for tkn in tokens:
-            if tkn in punctuations:
-                tokens.remove(tkn)
+        article = article.lower(article)
+        article = article.replace('{html}', "")
+        cleans = re.compile('<.*?>')
+        cleantext = re.sub(cleans, '', article)
+        rem_url = re.sub(r'http\S+', '', cleantext)
+        rem_num = re.sub('[0-9]+', '', rem_url)
+        tokenizer = RegexpTokenizer(r'\w+')
+        tokens = tokenizer.tokenize(rem_num)
+        insert_tokenized(tokens, link)
         no_stopwords = [word.lower() for word in tokens if not word in stopwords.words()]
         tagged = nltk.pos_tag(no_stopwords)
         entities = nltk.chunk.ne_chunk(tagged)
