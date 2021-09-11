cb0539988764e9b66b484ba12cb92141f7743310
Eva Ntourou <evedour@gmail.com>
2021-09-11 03:02:37 +0300

    index had duplicates

--- Scraper/NewsScrape/indexer.py
+++ Scraper/NewsScrape/indexer.py
@@ -2,28 +2,35 @@ import math
 import xml.etree.cElementTree as ET
 import json
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from progressbar import ProgressBar
 
 
 def make_index(dictionary, cor, links):
+    pbar = ProgressBar()
     # count frequencies and construct inverted index
     with open('Results/inverted_index.xml', 'w') as index_out:
         print('Making xml file for inverted index')
         root = ET.Element("inverted_index")
-        for lemma in list(lemmas for lemmas in dictionary.token2id):
-            lemma_name = ET.SubElement(root, "lemma", name=lemma)
-            lemma_id = dictionary.token2id[lemma]
+        lemma_list = list(dictionary.token2id.keys())
+        lemma_id_list = list(dictionary.token2id.values())
+        for lemma_id in pbar(range(len(dictionary.token2id))):
+            idx = lemma_id_list.index(lemma_id)
+            lemma_n = lemma_list[idx]
+            lemma_name = ET.SubElement(root, "lemma", name=lemma_n)
             i = 0
+
             for key in cor:
                 doc_id=links[i]
                 i += 1
-                for pair in key:
-                    if pair[0] == lemma_id:
-                        tf = pair[0] / len(key)
-                        break
+                r = [item for item in key if item[0] == lemma_id]
+                if len(r) == 0:
+                    tf = 0
+                else:
+                    tf = r[0][0] / len(key)
                 idf = dictionary.num_docs / dictionary.dfs[lemma_id]
                 lemma_weight = tf * idf
                 document = ET.SubElement(lemma_name, "document", id=doc_id, weight=str(lemma_weight))
 
-            tree = ET.ElementTree(root)
-            tree.write('Results/inverted_index.xml')
+        tree = ET.ElementTree(root)
+        tree.write('Results/inverted_index.xml')
     print('Successfully created index. It can be found in the Results folder')
